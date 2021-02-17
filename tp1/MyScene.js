0d333736179b44c5f91ca6eccf108013cdd55c77
Vasco Gomes <up201906617@fe.up.pt>
2021-02-17 16:50:53 +0000

    TP1 - Exercício 1.1

--- tp1/MyScene.js
+++ tp1/MyScene.js
@@ -1,5 +1,6 @@
 import { CGFscene, CGFcamera, CGFaxis } from "../lib/CGF.js";
 import { MyDiamond } from "./MyDiamond.js";
+import { MyTriangle } from "./MyTriangle.js";
 
 /**
  * MyScene
@@ -26,6 +27,7 @@ export class MyScene extends CGFscene {
     //Initialize scene objects
     this.axis = new CGFaxis(this);
     this.diamond = new MyDiamond(this);
+    this.triangle = new MyTriangle(this);
 
     //Objects connected to MyInterface
     this.displayAxis = true;
@@ -92,6 +94,7 @@ export class MyScene extends CGFscene {
     // ---- BEGIN Primitive drawing section
 
     this.diamond.display();
+    this.triangle.display();
 
     // ---- END Primitive drawing section
   }
