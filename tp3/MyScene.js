c021c34394166231119e8c354bbcff3f2a296e1f
Mariana Monteiro <up202003480@fe.up.pt>
2021-03-05 18:11:27 +0000

    TP3 - Exercício 4

--- tp3/MyScene.js
+++ tp3/MyScene.js
@@ -133,6 +133,13 @@ export class MyScene extends CGFscene {
         this.material3.setSpecular(1, 0, 0, 1.0);
         this.material3.setShininess(10.0);
 
+        // Wood
+        this.material4 = new CGFappearance(this);
+        this.material4.setAmbient(0.7, 0.3, 0.0, 1.0);
+        this.material4.setDiffuse(1, 1, 1, 1.0);
+        this.material4.setSpecular(0.2, 0.2, 0.2, 1.0);
+        this.material4.setShininess(10.0);
+        
         // Custom material (can be changed in the interface)
         // initially midrange values on ambient, diffuse and specular, on R, G and B respectively
 
@@ -146,10 +153,10 @@ export class MyScene extends CGFscene {
 
         this.updateCustomMaterial();
 
-        this.materials = [this.material1, this.material2, this.material3, this.customMaterial];
+        this.materials = [this.material1, this.material2, this.material3, this.customMaterial, this.material4,];
 
         // Labels and ID's for object selection on MyInterface
-        this.materialIDs = {'Red Ambient': 0, 'Red Diffuse': 1, 'Red Specular': 2, 'Custom': 3 };
+        this.materialIDs = {'Red Ambient': 0, 'Red Diffuse': 1, 'Red Specular': 2, 'Custom': 3, 'Wood': 4 };
     }
     display() {
         // ---- BEGIN Background, camera and axis setup
